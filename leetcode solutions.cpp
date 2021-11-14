9c16640f66053bfe4c44357774757219376a564a
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-14 17:56:51 +0100

    Update leetcode solutions.cpp

--- leetcode solutions.cpp
+++ leetcode solutions.cpp
@@ -79,3 +79,54 @@ public:
         return sum;
     }
 };
+
+// https://leetcode.com/problems/regular-expression-matching/submissions/
+
+// smallest solution
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return regex_match(s, regex(p));
+    }
+};
+
+// normal solution 
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(p.empty())
+            return s.empty();
+        bool fchar = (!s.empty() && ((p[0]==s[0]) || (p[0]=='.')));
+        if(p.length()>=2 && p[1]=='*')
+            return (isMatch(s, p.substr(2))||(fchar && isMatch(s.substr(1), p)));
+            else
+            return fchar && isMatch(s.substr(1), p.substr(1));
+    }
+};
+
+// big solution
+class Solution {
+    bool dp(int i, int j, string s, string p, vector<vector<int>> &memo)
+    {
+        if(memo[i][j]!=-1)
+            return memo[i][j] == 1;
+        bool ans;
+        if(j==p.length())
+            ans = i == s.length();
+        else
+        {
+            bool fchar =  (i<s.length() && (p[j]==s[i] || p[j]=='.'));
+            if(j+1<p.length() && p[j+1]=='*')
+                ans = (dp(i, j+2, s, p, memo)||(fchar && dp(i+1, j, s, p, memo)));
+            else
+                ans = fchar && dp(i+1, j+1, s, p, memo);
+        }
+        memo[i][j] = ans?1:0;
+        return ans;
+    }
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<int>> memo(s.length()+1, vector<int>(p.length()+1, -1));
+        return dp(0, 0, s, p, memo);
+    }
+};
