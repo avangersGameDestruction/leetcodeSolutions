1538a0d06f20e83c8b8799fb397c4c7c0fd134a4
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-19 15:51:11 +0100

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -81,3 +81,48 @@ var reverseKGroup = function(head, k) {
     }
     return newNode.next;
 };
+
+// sudoku solver
+var solveSudoku = function(board) {
+    function sudokuSolver(board) {
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (board[i][j] === '.') {
+                    let char = '1';
+                    while (char <= 9) {
+                        if (isValidSudoku(i, j, char)) {
+                            board[i][j] = char;
+
+                            if (sudokuSolver(board)) return true;
+                            else board[i][j] = '.';
+                        }
+                        char = (parseInt(char) + 1).toString();
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    function isValidSudoku(row, col, char) {
+        for (let i = 0; i < 9; i++) {
+            if (board[row][i] === char) return false;
+        }
+
+        for (let i = 0; i < 9; i++) {
+            if (board[i][col] === char) return false;
+        }
+
+        let x = Math.floor(row / 3) * 3;
+        let y = Math.floor(col / 3) * 3;
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[x + i][y + j] === char) return false;
+            }
+        }
+        return true;
+    }
+    sudokuSolver(board);
+};
