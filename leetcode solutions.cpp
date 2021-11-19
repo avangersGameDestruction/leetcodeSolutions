5084428111ac6511d5255fb9d538d4e56d2d7365
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-19 15:43:32 +0100

    Update leetcode solutions.cpp

--- leetcode solutions.cpp
+++ leetcode solutions.cpp
@@ -130,3 +130,17 @@ public:
         return dp(0, 0, s, p, memo);
     }
 };
+
+// hammingDistance
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res = 0;
+        int n = x ^ y;
+        while (n) {
+            res += n & 1;
+            n >>= 1;
+        }
+        return res;
+    }
+};
